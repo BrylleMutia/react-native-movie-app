cf4d0bd4224a157b338e739ca9c3e4ff2c7383a0
bryllemutia <bryllemutia3@gmail.com>
2025-08-15 13:33:03 +0800

    added secondary pages and tabs layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,10 @@
 import { Text, View } from "react-native";
-import "./global.css";
+import "../globals.css";
 
 export default function App() {
    return (
       <View className="flex-1 items-center justify-center bg-white">
-         <Text className="text-xl font-bold text-blue-500">
-            Welcome to Nativewfdsafsind!
-         </Text>
+         <Text className="text-5xl font-bold text-accent">Movie App!!</Text>
       </View>
    );
 }
