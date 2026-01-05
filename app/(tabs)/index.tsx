09593c98731e8d1f838bf655d8657bed15f51651
bryllemutia <bryllemutia3@gmail.com>
2026-01-05 10:55:32 +0800

    updated expo and dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import "../globals.css";
 
 export default function App() {
    return (
-      <View className="flex-1 items-center justify-center bg-white">
-         <Text className="text-5xl font-bold text-accent">Movie App!!</Text>
+      <View>
+         <Text>Home</Text>
       </View>
    );
 }
