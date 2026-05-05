d6c64132526b0fc9baf0f759c292ed4b75cec569
BrylleMutia <bryllemutia3@gmail.com>
2026-05-05 14:10:13 +0800

    feat: trending movies based on user search terms

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,20 +3,22 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import {
-   ActivityIndicator,
-   FlatList,
-   Image,
-   Text,
-   View
-} from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../globals.css";
 
 export default function App() {
    const router = useRouter();
+
+   const {
+      data: trendingMovies,
+      loading: trendingMoviesLoading,
+      error: trendingMoviesError,
+   } = useFetch(() => getTrendingMovies());
+
    const {
       data: movies,
       loading: moviesLoading,
@@ -27,21 +29,19 @@ export default function App() {
       <View className="flex-1 bg-primary">
          <Image source={images.bg} className="absolute w-full z-0" />
 
-         <View
-            className="flex-1 px-5 min-h-full"
-         >
+         <View className="flex-1 px-5 min-h-full">
             <Image
                source={icons.logo}
                className="w-12 h-10 mt-20 mb-5 mx-auto"
             />
 
-            {moviesLoading ? (
+            {moviesLoading || trendingMoviesLoading ? (
                <ActivityIndicator
                   size="large"
                   color="#0000ff"
                   className="mt-10 self-center"
                />
-            ) : moviesError ? (
+            ) : moviesError || trendingMoviesError ? (
                <Text>Error: {moviesError?.message}</Text>
             ) : (
                <View className="flex-1 mt-5">
@@ -49,24 +49,47 @@ export default function App() {
                      onPress={() => router.push("/search")}
                      placeholder="Search"
                   />
-                  <SafeAreaView style={{flex: 1}}>
-                    <FlatList
+
+                  {trendingMovies && (
+                     <View className="mt-10">
+                        <Text className="text-lg text-white font-bold mb-3">
+                           Trending Movies
+                        </Text>
+                     </View>
+                  )}
+
+                  <SafeAreaView style={{ flex: 1 }}>
+                     <FlatList
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        ItemSeparatorComponent={() => <View className="w-4" />}
+                        className="mb-4 mt-3"
+                        data={trendingMovies}
+                        renderItem={({ item, index }) => (
+                           <Text className="text-white text-sm">
+                              {item.title}
+                           </Text>
+                        )}
+                        keyExtractor={(item) => item.movie_id.toString()}
+                     />
+                  </SafeAreaView>
+
+                  <SafeAreaView style={{ flex: 1 }}>
+                     <FlatList
                         data={movies}
                         ListHeaderComponent={
                            <Text className="text-lg text-white font-bold mt-5 mb-3">
                               Latest movies
                            </Text>
                         }
-                        renderItem={({ item }) => (
-                           <MovieCard {...item} />
-                        )}
+                        renderItem={({ item }) => <MovieCard {...item} />}
                         keyExtractor={(item) => item.id.toString()}
                         numColumns={3}
                         columnWrapperStyle={{
                            justifyContent: "flex-start",
                            gap: 20,
                            paddingRight: 5,
-                           marginBottom: 10 
+                           marginBottom: 10,
                         }}
                         className="mt-2 pb-32"
                      />
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,11 +5,16 @@ import { Image, TextInput, View } from "react-native";
 interface SearchBarProps {
    placeholder: string;
    onPress?: () => void;
-   value: string;
-   onChangeText: (text: string) => void;
+   value?: string;
+   onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, value, onChangeText, onPress }: SearchBarProps) => {
+const SearchBar = ({
+   placeholder,
+   value,
+   onChangeText,
+   onPress,
+}: SearchBarProps) => {
    return (
       <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
          <Image
