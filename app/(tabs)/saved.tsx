2f56b43539f234c84b0b61318e29fe75dd5864d2
BrylleMutia <bryllemutia3@gmail.com>
2026-07-07 14:25:15 +0800

    feat: initial saved section

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,18 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
 
 const Saved = () => {
    return (
-      <View>
-         <Text>Saved</Text>
+      <View className="bg-primary flex-1">
+         <View className="flex-row items-center justify-between flex-1 flex-col">
+            <Image source={images.bg} className="absolute w-full z-0" />
+            <Image
+               source={icons.logo}
+               className="w-12 h-10 mt-20 mb-5 mx-auto"
+            />
+         </View>
       </View>
    );
 };
